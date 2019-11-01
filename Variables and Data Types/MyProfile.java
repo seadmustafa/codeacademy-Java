8e5a3da46e2502e16cc6b44ea6f18655e88b63fd
Sead <seadmustafa@users.noreply.github.com>
2019-11-01 12:06:56 +0300

    Create MyProfile.java

---
+++ Variables and Data Types/MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+  String name = "myName";
+    int age = 80;
+    double desiredSalary=85.5;
+    char gender='m';
+    boolean lookingForJob=false;
+	}
+}
