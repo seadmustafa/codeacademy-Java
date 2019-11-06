39bb575c4231aace770da4984b9715ce720cd078
Sead <seadmustafa@users.noreply.github.com>
2019-11-06 21:15:21 +0300

    String Methods add folder

---
+++ String Methods/CombineNames.java
@@ -0,0 +1,15 @@
+public class CombineNames {
+  
+	public static void main(String[] args) {
+    
+    // Add your first names:
+    String firstName = "Sia ";   
+    String lastName = "Furler";
+    
+    // What's the full name?
+    System.out.println( firstName.concat(lastName));
+    
+  }
+  
+}
+
